dd9555b817a1e93f580c665fb415f863f6a4b946
n10994181 <n10994181@qut.edu.au>
2021-11-04 22:56:29 +1000

    Erros Updated

--- website/main/errors.py
+++ website/main/errors.py
@@ -8,19 +8,19 @@ def not_found_error(error):
     
 @app.errorhandler(403)
 def not_found_error(error):
-    return render_template('403.html'), 404
+    return render_template('403.html'), 403
 
 @app.errorhandler(402)
 def internal_error(error):
-    return render_template('402.html'), 500
+    return render_template('402.html'), 402
 
 @app.errorhandler(401)
 def internal_error(error):
-    return render_template('401.html'), 500
+    return render_template('401.html'), 401
     
 @app.errorhandler(400)
 def internal_error(error):
-    return render_template('400.html'), 500
+    return render_template('400.html'), 400
 
 @app.errorhandler(500)
 def internal_error(error):
