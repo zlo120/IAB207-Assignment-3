6dace8cfc3f5fdcfff0ecf0bee446fde9f7708e9
n10994181 <n10994181@qut.edu.au>
2021-11-04 15:06:33 +1000

    Added Events to Home Page

--- website/views.py
+++ website/views.py
@@ -1,7 +1,25 @@
 from flask import Blueprint, render_template
+from threading import current_thread
+from flask import Blueprint, abort
+from flask_login import current_user
+from flask.helpers import url_for
+from flask_login.utils import login_required
+from werkzeug.utils import redirect, secure_filename
+import os
+from datetime import datetime
+
+from website.auth import login
+
+from . import db
+from .models import Event, Category, Comment, Order, User 
+
 bp = Blueprint('main', __name__)
 
 
 @bp.route('/')
 def index():
-    return render_template("index.html")
+
+    events = Event.query.all()
+    categories = Category.query.all()
+
+    return render_template("index.html", events = events, categories = categories)
