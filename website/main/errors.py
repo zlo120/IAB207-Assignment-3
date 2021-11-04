45de1c1c0c80d971dae5bff85b650c04b9ee184a
n10994181 <n10994181@qut.edu.au>
2021-11-04 11:42:51 +1000

    Error Handling, Added 401

--- website/main/errors.py
+++ website/main/errors.py
@@ -5,6 +5,22 @@ from flask import app
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
+    
+@app.errorhandler(403)
+def not_found_error(error):
+    return render_template('403.html'), 404
+
+@app.errorhandler(402)
+def internal_error(error):
+    return render_template('402.html'), 500
+
+@app.errorhandler(401)
+def internal_error(error):
+    return render_template('401.html'), 500
+    
+@app.errorhandler(400)
+def internal_error(error):
+    return render_template('400.html'), 500
 
 @app.errorhandler(500)
 def internal_error(error):
